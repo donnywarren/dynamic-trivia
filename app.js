6a62b34b63e5793d44e6796d716784041936fd2c
donnywarren9 <donnywarren9@gmail.com>
2020-07-14 20:41:04 -0400

    evevent listeners on selection cube

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const enterBtn = document.querySelector("#enter-game")
-const welcomeScrn = document.querySelector("#welcome")
-const instructionsScrn = document.querySelector("#instructions")
+// const welcomeScrn = document.querySelector("#welcome")
+// const instructionsScrn = document.querySelector("#instructions")
 const categoryScrn = document.querySelector("#cateories")
+const categoryChoice = document.querySelector(".cubespinner")
+const gameScrn = document.querySelector("#game")
 
 enterBtn.addEventListener('click', (e) => {
   e.preventDefault()
   instructionsScrn.classList.add("raise-curtain")
+})
+
+categoryChoice.addEventListener('click', (e) => {
+  console.log(e.target.className)
 })
